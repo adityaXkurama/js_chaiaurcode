70778720eab35e6edc2d213c92ddc7445dc970c6
adityaXkurama <raybandarkaraditya@gmail.com>
2024-05-07 12:33:38 +0530

    if_else and switch

---
+++ 04_control_flow/one.js
@@ -0,0 +1,38 @@
+//if 
+const isUserLoggedIn=true
+const temperature=41
+// if(temperature===41){
+//     console.log("temp less than 50");
+// }else{
+//     console.log("temp greater than 50");
+// }
+// const score = 200
+// if(score>100){
+//     const power = "fly"
+//     console.log(`user power: ${power}`);
+// }
+// console.log(`user power: ${power}`);
+
+
+const balance = 1000
+// if(balance >500) console.log("test"); implicit code
+
+// if (balance < 500) {
+//     console.log("Less thn");
+// }else if(balance < 750){
+//     console.log("less thn 750");
+// }else{
+//     console.log("Less than 1200");
+// }
+
+const userLoggedIn=true
+const deitcard=true
+const LoggedInFromGoogle=false
+const LoggedInFromEmail=true
+// if(userLoggedIn && deitcard && ){
+//     console.log("allow to buy course");
+// }
+
+if (LoggedInFromGoogle||LoggedInFromEmail){
+    console.log("User logged in.");
+}
---
+++ 04_control_flow/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("february");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+
+    default:
+        console.log("whatever");
+        break;
+}
